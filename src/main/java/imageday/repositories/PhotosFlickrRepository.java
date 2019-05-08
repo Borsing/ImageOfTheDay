50ffa1cae790ca0efff92fcaf079ef9236f33981
Benjamin Robert <benji_robert@msn.com>
2019-05-08 16:42:29 +0200

    [Update] Moving Repository into package and Update queries   - Remove Rest API on repository   - Add queries following JPA Query methods keywords     - Add query to get on a specific day the publicated image     - Add query to get a collection of images after a specific day   - Remove previous useless queries

---
+++ src/main/java/imageday/repositories/PhotosFlickrRepository.java
@@ -0,0 +1,18 @@
+package imageday.repositories;
+
+import java.util.Date;
+import java.util.List;
+
+import imageday.entities.PhotoPublication;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotosFlickrRepository extends PagingAndSortingRepository<PhotoPublication, Long> {
+
+    PhotoPublication findByDayPublication(@Param("dayPublication")Date photoOfTheDay);
+
+    List<PhotoPublication> findByDayPublicationBefore(@Param("dayPublication")Date photoOfTheyDay);
+
+}
