16ec09dbf5cac286f6de816b0f519efa01f90217
Benjamin Robert <benji_robert@msn.com>
2019-05-08 16:49:38 +0200

    [Feature] Add basic exception for the API

---
+++ src/main/java/imageday/exceptions/PhotoPublicationException.java
@@ -0,0 +1,8 @@
+package imageday.exceptions;
+
+public class PhotoPublicationException extends Exception {
+
+    public PhotoPublicationException(String errorMessage){
+        super(errorMessage);
+    }
+}
